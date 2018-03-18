40f765f47bf851aba9dc898108753bf77c70e490
eth-p <32112321+eth-p@users.noreply.github.com>
2018-03-17 19:34:55 -0700

    Improved bot.
    - Now tweets at 08:00 EST
    - Twitter keys moved to environment vars.
    - Tournament database moved to json file.

--- bot.js
+++ bot.js
@@ -1,104 +1,117 @@
-console.log('twitter bot started');
+// Requires
+const twit = require('twit');
 
 
-// Twitter API setup
-// Using Twit (https://github.com/ttezel/twit)
-var Twit = require('twit');
+// URL Generators
+/**
+ * Generates a Twitch stream URL.
+ * @param streamer The streamer.
+ * @returns {string} The URL.
+ */
+const URL_TWITCH = (streamer) =>
+	streamer != null
+		? `https://www.twitch.tv/${streamer}` :
+		'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee';
 
-// Twitter API data goes here
-var T = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  })
 
-// Tourneydata for the year 2018
-//    TOURNEY DAY                               MONTH/DAY
-var tourneydata = [
-    ['EGLX 2018'                               ,'3','9', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['National Melee Arcadian / Midwest Mayhem','3','11', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['SWEET 31'                                ,'3','17', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['The Mango'                               ,'3','18', 'https://www.twitch.tv/mang0'],
-    ['Full Bloom 4'                            ,'3','24', 'https://www.twitch.tv/meleeeveryday'],
-    ['Respawn 6'                               ,'3','31', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['ESA Oakland Opener'                      ,'4','7' , 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Fight Pitt 8'                            ,'4','7', 'www.twitch.tv/pghsmash'],
-    ['The Deep 5'                              ,'4','14', 'http://twitch.tv/santacruzmelee'],
-    ['ConComics'                               ,'4','20', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Flatiron 3'                              ,'4','21', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Aegis'                                   ,'4','21', 'https://www.twitch.tv/meleeeveryday'],
-    ['Arcane Tournaments'                      ,'4','27', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Smash Summit 6'                          ,'5','3', 'https://www.twitch.tv/beyondthesummit'],
-    ['KVO 2018'                                ,'5','4', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Pound Underground'                       ,'5','12', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Push More Buttons 2018'                  ,'5','12', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['GOML 2018'                               ,'5','18', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['BAM 2018'                                ,'5','18', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['MomoCon'                                 ,'5','24', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Combo Breaker 2018 / EDEN'               ,'5','25', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Smash N Splash 2018'                     ,'6','1', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Dreamhack Austin'                        ,'6','1', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Saints Gaming Live'                      ,'6','9', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['OpTic Arena'                             ,'6','8', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Dreamhack Summer'                        ,'6','15', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Smashville 7'                            ,'6','16', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Omega II'                                ,'6','16', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Smashadelphia 2018'                      ,'6','22', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['CEO 2018'                                ,'6','28', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Awakening 4'                             ,'6','29', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['The Even Bigger Balc'                    ,'7','6', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Smash Factor 7'                          ,'7','20', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Low Tier City 6'                         ,'7','27', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['EVO 2018'                                ,'8','3', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Smashcon 2018'                           ,'8','9', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Heir 5'                                  ,'8','17', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Shine 2018'                              ,'8','24', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Dreamhack Montreal'                      ,'9','7', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Canada Cup'                              ,'10','26', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Dreamhack Atlanta'                       ,'11','16', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Dreamhack Winter'                        ,'11','30', 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee'],
-    ['Dont Park On The Grass 2018'             ,'12','15', 'https://www.twitch.tv/endgametv1']
-];
-
-function tweet(tourneydata, day, month) {
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var time = date.getUTCHours();
-
-    console.log(day);
-    console.log(time);
-    
-        if (time >= 17) {
-            for (i = 0; i < tourneydata.length; ++i) {
-                if (tourneydata[i][1] == month) {
-                    if (tourneydata[i][2] == day) {
-                        if (tourneydata[i+1][2] == day) {
-                            return (tourneydata[i][0] + ' and ' + tourneydata[i+1][0] + ' are starting today! ' + 'https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Melee');
-                        } else {
-                            return (tourneydata[i][0] + ' is starting today! ' + tourneydata[i][3]);
-                        }
-                    }
-                }
-            }
-        }
-
-    return '';
-
+// Config
+/**
+ * Gets a configuration option.
+ * If this option is missing, the process will exit.
+ * @param key The config key.
+ */
+function getConfig(key) {
+	if (process.env[key] == null) {
+		console.error(`Missing configuration key: ${key}`);
+		process.exit(1);
+	}
 }
 
-function sendtweet() {
-    T.post('statuses/update', { status: tweet(tourneydata) }, function(err, data, response) {
 
-    })
+// Setup: Twitter
+let twitter = new twit({
+	consumer_key: getConfig('TWITTER_CONSUMER_KEY'),
+	consumer_secret: getConfig('TWITTER_CONSUMER_SECRET'),
+	access_token: getConfig('TWITTER_ACCESS_TOKEN'),
+	access_token_secret: getConfig('TWITTER_ACCESS_TOKEN_SECRET'),
+	timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+});
 
-    function gotData(err, data, response) {
-    }
+// Setup: Database
+let tourneydata = require('./config/tournaments');
+tourneydata.forEach((tourney, index) => {
+	tourney.date = new Date(tourney.date);
+	console.log(`DB: ${tourney.name} -- ${tourney.date.toUTCString()}`);
+});
+
+// Main
+console.log('Twitter bot started.');
+console.log();
+
+/**
+ * Sends a tweet about a tournament.
+ * @param tourney The tournament info.
+ */
+function tweet(tourney) {
+	const msg = `${tourney.name} is starting today! ${URL_TWITCH(tourney.twitch)}`;
+
+	twitter.post('statuses/update', {status: msg}, function (err, data, response) {
+		if (err) {
+			console.error("FAILED TO SEND TWEET: " + msg);
+		} else {
+			tourney.sent = true;
+			console.log("SENT TWEET: " + msg);
+		}
+	});
 }
 
-setInterval(sendtweet, 500000);
+/**
+ * Checks for new tournaments coming up, and sends tweets if so.
+ */
+function update() {
+	let date = new Date("2018-03-18 12:00 EST");
+
+	let year = date.getUTCFullYear();
+	let day = date.getUTCDate();
+	let month = date.getUTCMonth();
+	let time = date.getUTCHours();
+	let anything = false;
+
+	for (let tourney of tourneydata) {
+		// Skip sent.
+		if (tourney.sent === true) continue;
+
+		// If date is ok...
+		if (tourney.date.getUTCFullYear() === year
+			&& tourney.date.getUTCMonth() === month
+			&& tourney.date.getUTCDate() === day) {
+
+			// Default to 8:00 AM EST.
+			let compare_time = tourney.date.getUTCHours();
+			if (tourney.date.getUTCHours() === 0) {
+				compare_time = 13;
+			}
+
+			// If hours are ok, tweet it.
+			if (time >= compare_time) {
+				anything = true;
+				tweet(tourney);
+			}
+		}
+	}
+
+	// Log.
+	if (anything) {
+		console.log('[update] Checked for tournaments, and tweeted!');
+	} else {
+		console.log('[update] Checked for tournaments.');
+	}
+}
+
+// Timer
+update();
+setInterval(update, 1000 * 60 * 60 /* One Hour */);
+
 
 
 
